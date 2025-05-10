96880646c649d22f959675d596fa31c77b2f8d5d
swati <swati>
2025-05-10 21:32:12 +0530

    add new java class lodge

---
+++ src/main/java/com/example/makemytrip/lodge.java
@@ -0,0 +1,17 @@
+
+package com.example.makemytrip;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class lodge {
+    @GetMapping("/lodge")
+    public String getData()
+    {
+        return "Please book your hotels from Indigo. kindly book tickets for NEW DELHI to Anywhere at 10% discount";
+
+    }
+}
